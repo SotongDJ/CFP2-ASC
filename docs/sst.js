153df6014760d125820c5871cb98e85dd0f25757
David Soh <ln@trth.nl>
2024-04-12 01:22:17 +0800

    feat: based on CFP2-BLG and CFP2-JST

---
+++ docs/sst.js
@@ -0,0 +1,7 @@
+final_title_str='CFP2-SST 《神仙補習班》播放室';
+final_root_path='https://xn--158h.xn--2os22eixx6na.xn--kpry57d/';
+final_artist_str='謝依霖、丹丹老師';
+show_apple=true;
+show_google=false;
+show_spotify=true;
+show_youtube=true;
