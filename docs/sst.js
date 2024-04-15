f61cd054a0ae1830aff7f572d2948dc554b7165c
David Soh <ln@trth.nl>
2024-04-15 16:52:05 +0000

    refactor: more modularize and change button list

--- docs/sst.js
+++ docs/sst.js
@@ -1,6 +1,20 @@
 final_title_str='CFP2-ASC 《神仙補習班》播放室';
 final_root_path='https://xn--158h.xn--2os22eixx6na.xn--kpry57d/';
 final_artist_str='謝依霖、丹丹老師';
+
+document.getElementById("aboutA").href="https://github.com/SotongDJ/CFP2-ASC";
+document.getElementById("emailA").href="mailto:github@stdj.me?subject=ASC 回饋信件&body=內容類型：（問題？建議？）%0A細節：（越詳細越好）";
+document.getElementById("infoA").href="https://github.com/SotongDJ/CFP2-ASC/wiki";
+document.getElementById("titleCSpan").innerText="神仙補習班";
+document.getElementById("titleEmoji").innerText="🚀";
+document.getElementById("titleESpan").innerText="ASC";
+document.getElementById("titleSec").href="https://player.soundon.fm/p/77c51a83-c5c5-460d-8363-0deec983efe1";
+document.getElementById("titleSec").title="神仙補習班";
+document.getElementById("sitleEmoji").innerText="🚀";
+document.getElementById("sitleESpan").innerText="ASC";
+document.getElementById("sitleSec").href="https://player.soundon.fm/p/77c51a83-c5c5-460d-8363-0deec983efe1";
+document.getElementById("sitleSec").title="神仙補習班";
+
 show_apple=true;
 show_google=false;
 show_spotify=true;
